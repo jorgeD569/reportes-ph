5fd7a88155a53eea601f7a00de6b8474042d2fe4
Jorge Dabrowski <jorge.dabrowski@kompasssrl.com.ar>
2026-04-25 16:59:00 -0300

    servir carpeta frontend

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(express.static('FRONTEND'))
 
 // Healthcheck para que no tire "Cannot GET /"
 app.get('/', (req, res) => {
