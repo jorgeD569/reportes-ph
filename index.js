0e93e8cd33d7304c85faa066ae076c3a0b93320b
Jorge Dabrowski <jorge.dabrowski@kompasssrl.com.ar>
2026-04-25 17:11:31 -0300

    fix frontend render path

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
-app.use(express.static('FRONTEND'))
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'FRONTEND')))
 
 // Healthcheck para que no tire "Cannot GET /"
 app.get('/', (req, res) => {
